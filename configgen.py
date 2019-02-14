d3e1fdfc232f14161106a27c56bacf05e645dddd
ICEFIR <icefir1996@gmail.com>
2019-02-14 18:32:11 +1100

    fix double quotes

--- configgen.py
+++ configgen.py
@@ -7,7 +7,7 @@ hashed = passwd(os.environ['jupyter_password'])
 base_url = os.environ['base_url']
 with open(config_path,'r') as f:
   content = f.read()
-overwrite_base_url = re.sub(r"^.*c\.NotebookApp\.base_url .*\n", f"c.NotebookApp.base_url = '{base_url}'",content,flags=re.M)
+overwrite_base_url = re.sub(r"^.*c\.NotebookApp\.base_url .*\n", f"c.NotebookApp.base_url = {base_url}",content,flags=re.M)
 overwrite_password = re.sub(r"^.*c\.NotebookApp\.password .*\n", f"c.NotebookApp.password = u'{hashed}'",overwrite_base_url,flags=re.M)
 
 #  print (overwrite_password)

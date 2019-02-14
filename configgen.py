a706b73cda6a32a6853246781a3b44b3e6a832e7
ICEFIR <icefir1996@gmail.com>
2019-02-14 18:12:34 +1100

    fix in script

--- configgen.py
+++ configgen.py
@@ -7,7 +7,7 @@ hashed = passwd(os.environ['jupyter_password'])
 base_url = os.environ['base_url']
 with open(config_path,'r') as f:
   content = f.read()
-overwite = re.sub(r"^.*c\.NotebookApp\.baseurl .*\n", f"c.NotebookApp.baseurl = '{base_url}'",content,flags=re.M)
+overwite = re.sub(r"^.*c\.NotebookApp\.base_url .*\n", f"c.NotebookApp.base_url = '{base_url}'",content,flags=re.M)
 overwite = re.sub(r"^.*c\.NotebookApp\.password .*\n", f"c.NotebookApp.password = u'{hashed}'",content,flags=re.M)
 
 #  print(re.findall(r"^.*c\.NotebookApp\.password .*\n",content,flags=re.M))
